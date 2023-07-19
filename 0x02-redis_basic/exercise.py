4ddf979a4867162c6247a377d7bf03f03bafb6ef
Aymen <aymenabduseid10@gmail.com>
2023-07-19 16:14:16 -0400

    hot fix

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+""" Redis basic """
+import redis
+import sys
+from uuid import uuid4
+from typing import Callable, Any, Optional, Union
+from functools import wraps
+
+
+Types = Union[str, bytes, int, float]
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    """
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        """
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """
+    """
+    key = method.__qualname__
+    i = "".join([key, ":inputs"])
+    o = "".join([key, ":outputs"])
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """ """
+        self._redis.rpush(i, str(args))
+        res = method(self, *args, **kwargs)
+        self._redis.rpush(o, str(res))
+        return res
+
+    return wrapper
+
+
+class Cache:
+    """
+    """
+
+    def __init__(self):
+        """
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    @count_calls
+    @call_history
+    def store(self, data: Types) -> str:
+        """
+        """
+        key = str(uuid4())
+        self._redis.mset({key: data})
+        return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) \
+            -> Types:
+        """
+        """
+        if fn:
+            return fn(self._redis.get(key))
+        data = self._redis.get(key)
+        return data
+
+    def get_int(self: bytes) -> int:
+        """
+        """
+        return int.from_bytes(self, sys.byteorder)
+
+    def get_str(self: bytes) -> str:
+        """
+        """
+        return self.decode("utf-8")
